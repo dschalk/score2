c56a2b852ace24f0e2fda971069d44ee930ccd12
David Schalk <pyschalk@gmail.com>
2015-02-24 17:45:31 -0800

    Code overhaul.

--- Main.hs
+++ Main.hs
@@ -197,7 +197,8 @@ talk conn state (user, _, _) = forever $ do
     else if "CC#$42" `T.isPrefixOf` msg || "CE#$42" `T.isPrefixOf` msg || "CF#$42" `T.isPrefixOf` msg || 
         "CH#$42" `T.isPrefixOf` msg || "CJ#$42" `T.isPrefixOf` msg || "CK#$42" `T.isPrefixOf` msg || 
         "CO#$42" `T.isPrefixOf` msg || "CP#$42" `T.isPrefixOf` msg || "CQ#$42" `T.isPrefixOf` msg || 
-        "CY#$42" `T.isPrefixOf` msg || "CR#$42" `T.isPrefixOf` msg
+        "CY#$42" `T.isPrefixOf` msg || "CR#$42" `T.isPrefixOf` msg || "CD#$42" `T.isPrefixOf` msg || 
+        "CF#$42" `T.isPrefixOf` msg
         then 
             do 
                 st <- readMVar state 
